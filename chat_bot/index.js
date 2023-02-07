ab7eed21586738c33cf6ba67ae86d0d5cba8174a
Akhilesh K M <kmakhil294@gmail.com>
2023-02-07 22:54:27 +0530

    created simple react app and needs to be updated

---
+++ chat_bot/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+
