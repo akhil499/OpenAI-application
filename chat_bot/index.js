b3e5d68e6bfccf0bd1771eb932c6494a29b6af2d
Akhilesh K M <kmakhil294@gmail.com>
2023-02-12 20:12:49 +0530

    completing the project. added .env file udated index.js and app.js to make the chat_bot work as expected

--- chat_bot/index.js
+++ chat_bot/index.js
@@ -1,18 +1,44 @@
+require('dotenv').config(); // required to use env
+
+const OpenAI = require('openai'); // code reference https://platform.openai.com/docs/api-reference/introduction
+const { Configuration, OpenAIApi } = OpenAI
+const { REACT_APP_OPENAI_API_KEY } = process.env;
+const { REACT_APP_AUTHORIZATION } = process.env;
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const port =  3001;
 const app = express();
+
+const configuration = new Configuration({
+  organization: REACT_APP_AUTHORIZATION,
+  apiKey: REACT_APP_OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+app.post('/', async (req, res) => {
+  const { message } = req.body;
+  const response = await openai.createCompletion({
+    "model": "text-davinci-003",
+    "prompt": `Pretend you are Tom and answer all the questions.
+    Tom: How can I help you ?
+    Person: What is your name ?
+    Tom: My name is Tom.
+    Person: ${message}?
+    Tom:`,
+    "max_tokens": 100,
+    "temperature": 0
+  });
+  console.log(response.data)
+  if(response.data.choices[0].text) {
+    res.json({
+      message: response.data.choices[0].text
+    });
+  }
 });
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
-
 
--- chat_bot/src/App.js
+++ chat_bot/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+//create a react componant that inputs a textarea message then performs a fetch request to localhost:3001 gets back a response as a data.message and displayes that message ina box below
+
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+
+  //when the submit is hit calling the endpoint running at port 3001
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3001", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    })
+    .then((res) => res.json())
+    .then((data) => setResponse(data.message));
+    
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ChatBot Tom</h1>
+      <form onSubmit={handleSubmit}>
+        <textarea value={message} placeholder="Ask Tom anything" onChange={(e) => setMessage(e.target.value)} />
+       
+        <button type="submit">Submit</button>
+      </form>
+      {response && <div><b>Tom:</b> {response} </div> }
     </div>
   );
 }
 
-export default App;
+export default App
